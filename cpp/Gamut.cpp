86dfbbc24f6c6353d9c6a0ea1bd7ef58293f4728
Ante Poljicak <ante.poljicak@gmail.com>
2020-07-09 12:10:26 +0200

    Fix inequality conditions in Gamut.cpp
    The inequality conditions in Gamut::GetOctand() method
    overlooked the possibility to have a point exactly on
    the edge of the octand segment.

--- cpp/Gamut.cpp
+++ cpp/Gamut.cpp
@@ -171,19 +171,19 @@ void Gamut::PrintProgress(pfnPrintProgress PrintCallback, size_t current, size_t
 
 int Gamut::GetOctand(vec Lab_pos)
 {
-	if (Lab_pos(0) < 0 && Lab_pos(1) < 0 && Lab_pos(2) < 0)
+	if (Lab_pos(0) <= 0 && Lab_pos(1) <= 0 && Lab_pos(2) <= 0)
 		return 1;
-	else if (Lab_pos(0) < 0 && Lab_pos(1) < 0 && Lab_pos(2) > 0)
+	else if (Lab_pos(0) <= 0 && Lab_pos(1) <= 0 && Lab_pos(2) >= 0)
 		return 2;
-	else if (Lab_pos(0) < 0 && Lab_pos(1) > 0 && Lab_pos(2) < 0)
+	else if (Lab_pos(0) <= 0 && Lab_pos(1) >= 0 && Lab_pos(2) <= 0)
 		return 3;
-	else if (Lab_pos(0) < 0 && Lab_pos(1) > 0 && Lab_pos(2) > 0)
+	else if (Lab_pos(0) <= 0 && Lab_pos(1) >= 0 && Lab_pos(2) >= 0)
 		return 4;
-	else if (Lab_pos(0) > 0 && Lab_pos(1) < 0 && Lab_pos(2) < 0)
+	else if (Lab_pos(0) >= 0 && Lab_pos(1) <= 0 && Lab_pos(2) <= 0)
 		return 5;
-	else if (Lab_pos(0) > 0 && Lab_pos(1) < 0 && Lab_pos(2) > 0)
+	else if (Lab_pos(0) >= 0 && Lab_pos(1) <= 0 && Lab_pos(2) >= 0)
 		return 6;
-	else if (Lab_pos(0) > 0 && Lab_pos(1) > 0 && Lab_pos(2) < 0)
+	else if (Lab_pos(0) >= 0 && Lab_pos(1) >= 0 && Lab_pos(2) <= 0)
 		return 7;
 	else
 		return 8;
